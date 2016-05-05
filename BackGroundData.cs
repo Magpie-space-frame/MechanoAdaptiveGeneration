af6035a6d019708ea086f137e189c83bd777e0f6
Alessandro Felder <alessandrofelder@users.noreply.github.com>
2016-05-05 13:21:29 +0100

    Fixing runtime errors of dll
    Manage to compile in VS very easily (nice one, Harri)
    
    In the reset part, instead of clearing uninitialized lists, I assign them to a new empty list.
    
    Remaining runtime error is "Invalid cast: Number >> Int32"
    
    Made a gh file to test component

---
+++ BackGroundData.cs
@@ -0,0 +1,27 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanoAdaptiveGeneration
+{
+    public struct BackGroundData
+    {
+        public List<Point3d> G;
+        public List<double> T0;
+        public List<double> T1;
+        public List<double> T2;
+        public List<double> T3;
+        public List<double> T4;
+        public List<double> T5;
+
+        public int xCount;
+        public int yCount;
+        public int zCount;
+        public double xSize;
+        public double ySize;
+        public double zSize;
+    }
+}
